7a966d55afc68068be2840fdbd922ef80ef5f762
OlegZ <beyondswamps@gmail.com>
2022-12-04 20:50:49 +0300

    kata 7.2.11 done kata 7.2.12 just diving into

---
+++ src/kata7112/Program.java
@@ -0,0 +1,13 @@
+package kata7112;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
+        String phrase = sc.nextLine();
+        Arrays.stream(phrase.toLowerCase().split()).
+    }
+}

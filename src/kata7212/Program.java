803cb3123a28c68c126327e7159bc2644bc23dd4
OlegZ <beyondswamps@gmail.com>
2022-12-06 17:19:56 +0300

    Task 7.2.12 done even better (with only streams)

--- src/kata7212/Program.java
+++ src/kata7212/Program.java
@@ -1,10 +1,6 @@
 package kata7212;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -13,14 +9,14 @@ import java.util.stream.Collectors;
 public class Program {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in, "UTF-8").useDelimiter("[^A-Za-z0-9А-Яа-я]+");
-        Map<String, Long> rateMap = sc.tokens()
+        sc.tokens()
                 .map(String::toLowerCase)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        rateMap
-                .keySet()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
                 .stream()
-                .sorted()
-                .sorted((str1, str2) -> (int) (rateMap.get(str2) - rateMap.get(str1)))
+                .sorted(Map.Entry.comparingByKey())
+                .sorted((entry1, entry2) -> (int) (entry2.getValue() - entry1.getValue()))
+                .map(Map.Entry::getKey)
                 .limit(10)
                 .forEach(System.out::println);
     }

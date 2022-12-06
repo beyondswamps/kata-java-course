fc3f8f12059c340c592868223070fcc3fd593069
OlegZ <beyondswamps@gmail.com>
2022-12-06 16:04:26 +0300

    Task 7.2.12 done

--- src/kata7212/Program.java
+++ src/kata7212/Program.java
@@ -3,8 +3,7 @@ package kata7212;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.text.CollationElementIterator;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,15 +12,15 @@ import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in, "UTF-8");
-//        String phrase = sc.next();
-        Map<String, Long> result = Arrays.stream(sc.nextLine().toLowerCase().split("[^A-Za-z0-9А-Яа-я]+"))
+        Scanner sc = new Scanner(System.in, "UTF-8").useDelimiter("[^A-Za-z0-9А-Яа-я]+");
+        Map<String, Long> rateMap = sc.tokens()
+                .map(String::toLowerCase)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        result
+        rateMap
                 .keySet()
                 .stream()
                 .sorted()
-                .sorted((str1, str2) -> (int) (result.get(str2) - result.get(str1)))
+                .sorted((str1, str2) -> (int) (rateMap.get(str2) - rateMap.get(str1)))
                 .limit(10)
                 .forEach(System.out::println);
     }

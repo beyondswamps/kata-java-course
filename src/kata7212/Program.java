97b6ccec3d3c011b3072c006e1d94665493707a4
OlegZ <beyondswamps@gmail.com>
2022-12-07 10:32:34 +0300

    Task 7.2.12 done even better. Sort by values

--- src/kata7212/Program.java
+++ src/kata7212/Program.java
@@ -1,6 +1,7 @@
 package kata7212;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -8,14 +9,14 @@ import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in, "UTF-8").useDelimiter("[^A-Za-z0-9А-Яа-я]+");
+        Scanner sc = new Scanner(System.in, "UTF-8").useDelimiter("[^A-Za-z\\dА-Яа-я]+");
         sc.tokens()
                 .map(String::toLowerCase)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByKey())
-                .sorted((entry1, entry2) -> (int) (entry2.getValue() - entry1.getValue()))
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .map(Map.Entry::getKey)
                 .limit(10)
                 .forEach(System.out::println);

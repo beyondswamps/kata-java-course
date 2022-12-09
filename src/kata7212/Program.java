0515f80e2175b3dd3a980e0b552655cef6535179
OlegZ <beyondswamps@gmail.com>
2022-12-09 15:08:14 +0300

    Task 7.2.12 BufferedReader way

--- src/kata7212/Program.java
+++ src/kata7212/Program.java
@@ -1,17 +1,20 @@
 package kata7212;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in, "UTF-8").useDelimiter("[^A-Za-z\\dА-Яа-я]+");
-        sc.tokens()
-                .map(String::toLowerCase)
+        new BufferedReader(new InputStreamReader(System.in, "UTF-8"))
+                .lines()
+                .map(str -> str.split("[^\\wА-Яа-яËё+]"))
+                .flatMap(Stream::of)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
